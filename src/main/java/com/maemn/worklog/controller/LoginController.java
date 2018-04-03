12303617e4da3268a2273e05a5db55e681b881a1
iMaeGoo <mail1st@qq.com>
2018-04-03 18:44:34 +0800

    Temp commit, will test pom.xml later.

--- src/main/java/com/maemn/worklog/controller/LoginController.java
+++ src/main/java/com/maemn/worklog/controller/LoginController.java
@@ -3,11 +3,12 @@ package com.maemn.worklog.controller;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
-@Path("/login")
+@Path("/rest/login")
 public class LoginController {
     @GET
-    @Produces("text/plain")
+    @Produces(MediaType.TEXT_PLAIN)
     public String getMessage() {
         return "Hello world!";
     }
