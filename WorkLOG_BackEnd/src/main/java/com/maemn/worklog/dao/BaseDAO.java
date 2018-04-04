9f7aff6536d2ce2d0d1a3f92921200c3d996b30c
iMaeGoo <mail1st@qq.com>
2018-04-04 16:13:19 +0800

    Issue: jersey in spring.

--- WorkLOG_BackEnd/src/main/java/com/maemn/worklog/dao/BaseDAO.java
+++ WorkLOG_BackEnd/src/main/java/com/maemn/worklog/dao/BaseDAO.java
@@ -1,6 +1,6 @@
 package com.maemn.worklog.dao;
 
-import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 public class BaseDAO extends HibernateDaoSupport {
 
