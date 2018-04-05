d783344c99a2ad1b19a84d0c3a608618029be157
iMaeGoo <mail1st@qq.com>
2018-04-05 18:26:37 +0800

    Use maven war plugin, fix UT failed.

--- WorkLOG_BackEnd/src/test/java/com/maemn/worklog/dao/TestUserDAO.java
+++ WorkLOG_BackEnd/src/test/java/com/maemn/worklog/dao/TestUserDAO.java
@@ -2,10 +2,15 @@ package com.maemn.worklog.dao;
 
 import com.maemn.worklog.entity.UserPO;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertNotNull;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:applicationContext.xml"})
 public class TestUserDAO {
 
     @Autowired
